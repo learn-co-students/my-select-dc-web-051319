780d880452d56f7002a7c0ea44c8fa3b5616efb5
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-26 16:57:38 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+ i = 0 
+ true_values = []
+ end_index = collection.size
+ while(i<end_index)
+ if(yield(collection[i]) == true)
+   true_values.push(collection[i])
+   i+=1
+ else
+   i+=1
+ end
+ end
+ return true_values
 end
