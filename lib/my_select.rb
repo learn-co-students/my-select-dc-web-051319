14acc48cb4115d2ed334194073aceee4a81eec15
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-01 21:53:56 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ tick = 0
+ arr2 = []
+ 
+ while tick < collection.length
+  if yield(collection[tick]) == true
+    arr2.push(collection[tick])
+  end
+  tick += 1
+ end
+ arr2
 end
