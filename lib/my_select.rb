9972b79236b9951529a568fbd61faa8384a11d30
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-10 14:09:42 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  arr = []
+  index = 0
+  w = collection.length
+  while w > 0
+    f = yield collection[index]
+    if f == true
+      arr.push(collection[index])
+    end
+    index += 1
+    w -= 1
+  end
+  arr
 end
