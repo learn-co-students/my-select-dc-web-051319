be0234c292af49601e1ab393cacbb0b978d55b55
Your Name <you@example.com>
2019-05-12 21:25:48 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  output = []
+  while i < collection.length
+    #yield if the collection has something at i then continue onto next part of the code.
+    output << collection[i] if yield(collection[i])
+    i += 1
+  end
+  output
 end
